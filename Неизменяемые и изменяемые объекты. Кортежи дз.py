e85005ad7442536f3b2fd1115bf54fb2f5336e5d
Vladislav36366 <mihailo04@mail.ru>
2024-08-25 11:08:32 +0500

    Add files via upload

---
+++ Неизменяемые и изменяемые объекты. Кортежи дз.py
@@ -0,0 +1,12 @@
+immutable_var = (1, 2, 'a', 'b')
+print(immutable_var)
+#immutable_var[0][0] = 3 #изменить значение нельзя, это неизменяемый обект, чтобы можно было изменить он должен быть такой формы: immutable_var=([1, 2], 'a', 'b')
+mutable_list = ([1, 2, 'a', 'b', 'Modified'])
+print(mutable_list)
+mutable_list[0] = 5
+mutable_list[1] = 8
+mutable_list[2] = 'd'
+mutable_list[3] = 'c'
+mutable_list[4] = 'improved'
+print(mutable_list)
+
