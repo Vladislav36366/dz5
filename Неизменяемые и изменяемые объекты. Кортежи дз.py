b052a93f71504ba61eb251685c3b89cfb5e02703
Vladislav36366 <mihailo04@mail.ru>
2024-08-25 11:09:21 +0500

    Update Неизменяемые и изменяемые объекты. Кортежи дз.py

--- Неизменяемые и изменяемые объекты. Кортежи дз.py
+++ Неизменяемые и изменяемые объекты. Кортежи дз.py
@@ -1,6 +1,6 @@
 immutable_var = (1, 2, 'a', 'b')
 print(immutable_var)
-#immutable_var[0][0] = 3 #изменить значение нельзя, это неизменяемый обект, чтобы можно было изменить он должен быть такой формы: immutable_var=([1, 2], 'a', 'b')
+immutable_var[0][0] = 3 #изменить значение нельзя, это неизменяемый обект, чтобы можно было изменить он должен быть такой формы: immutable_var=([1, 2], 'a', 'b')
 mutable_list = ([1, 2, 'a', 'b', 'Modified'])
 print(mutable_list)
 mutable_list[0] = 5
